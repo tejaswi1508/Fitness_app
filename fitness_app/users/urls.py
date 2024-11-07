343766b86847fd564e555a875ccb6be8c98fc3ef
tejaswi1508 <inboxofteju@gmail.com>
2024-11-07 08:07:44 +0530

    logout and dashboard setup

--- fitness_app/users/urls.py
+++ fitness_app/users/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('logout/',views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path('home/', views.home_view, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- fitness_app/users/views.py
+++ fitness_app/users/views.py
@@ -1,7 +1,8 @@
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 def login_view(request):
@@ -11,10 +12,14 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid username or password.')
     return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect to login page after logout
 
 def home_view(request):
     return render(request, 'users/home.html')
@@ -29,3 +34,7 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
