607db006c6e8983ea7e164ca3a30e65865773ae1
tejaswi1508 <inboxofteju@gmail.com>
2024-11-07 06:58:39 +0530

    Registration

--- fitness_app/users/urls.py
+++ fitness_app/users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
     path('home/', views.home_view, name='home'),
 ]
--- fitness_app/users/views.py
+++ fitness_app/users/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 def login_view(request):
@@ -17,3 +18,14 @@ def login_view(request):
 
 def home_view(request):
     return render(request, 'users/home.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new user
+            messages.success(request, 'Your account has been created! You can now log in.')
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
