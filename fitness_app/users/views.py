1a10951e4edc766803030dad2bc71a6893a4e527
tejaswi1508 <inboxofteju@gmail.com>
2024-12-02 12:09:03 +0530

    changing directory of dashboard html

--- fitness_app/users/views.py
+++ fitness_app/users/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -38,3 +38,15 @@ def register_view(request):
 @login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance = request.user)
+    
+    return render(request, 'profile.html',{'form':form})
